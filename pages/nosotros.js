7819dcaea5e9ecac2b05908be51e1f6d8041f914
Edith Romero <edithjohanaromero@gmail.com>
2022-04-25 06:56:54 -0300

    Primer commit

---
+++ pages/nosotros.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Link from 'next/link'
+import Layaout from './components/Layaout'
+
+const Nosotros = () => {
+  return (
+    <Layaout
+        pagina='Nosotros'
+    >
+        <h1>Desde Nosotros</h1>
+    </Layaout>
+  )
+}
+
+export default Nosotros
