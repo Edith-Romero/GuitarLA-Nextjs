3c4f660dd199e7fdb0dc6c9a2ff5a7e55e43d42d
Edith Romero <edithjohanaromero@gmail.com>
2022-08-23 05:48:44 -0300

    Funcionando con id

--- pages/blog.js
+++ pages/blog.js
@@ -1,13 +1,58 @@
-import Layaout from './components/Layaout'
+import Entrada from '../components/Entrada'
+import Layaout from '../components/Layaout'
+import styles from '../styles/Blog.module.css'
 
-const Blog = () => {
+
+const Blog = ({entradas}) => {
   return (
     <Layaout
         pagina='blog'
     >
-        <h1>Desde Blog</h1>
+      <main className='contenedor'>
+        <h2 className='heading'>Blog</h2>
+
+        <div className={styles.blog}>
+          {entradas.data.map(entrada =>(
+            <Entrada
+              key ={entrada.id}
+              entrada = {entrada}
+            />
+          ))}
+        </div>
+      </main>
     </Layaout>
   )
 }
 
+export async function getStaticProps() {
+
+  const url= `${process.env.API_URL}/api/blogs?populate=*`
+  const respuesta= await fetch(url)
+  const entradas = await respuesta.json()
+
+  return{
+    props:{
+      entradas
+    }
+  }
+}
+
 export default Blog
+
+// Este es el ejemplo de getServerSideProps: Construye la pagina en cada Request, es decir cada vez 
+// consulto una pagina el va a la API y construye una respuesta, eso causa un uso intensivo del 
+// servidor, por eso lo comentamos y usamos getStaticProps()  porque el blog es un
+// contenido estatico
+
+// export async function getServerSideProps() {
+
+//   const url= 'http://localhost:1337/api/blogs'
+//   const respuesta= await fetch(url)
+//   const entradas = await respuesta.json()
+
+//   return{
+//     props:{
+//       entradas
+//     }
+//   }
+// }
